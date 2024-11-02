139a31bf7024d336c99974794761563c0bd49318
whereami0404 <whereami2048@gmail.com>
2024-11-02 14:34:41 +0900

    VACGOM-23 feat: jwt 관련 프로퍼티 추가

---
+++ vacgom-api/src/main/kotlin/kr/co/vacgom/api/auth/jwt/JwtProperties.kt
@@ -0,0 +1,11 @@
+package kr.co.vacgom.api.auth.jwt
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix="jwt")
+class JwtProperties(
+    val issuer: String,
+    val secret: String,
+    val accessTokenExpirationSec: Long,
+    val refreshTokenExpirationSec: Long,
+)
