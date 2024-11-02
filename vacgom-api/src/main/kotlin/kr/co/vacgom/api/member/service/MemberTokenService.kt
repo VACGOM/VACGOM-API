2a7ab7ed67d9792e5d770bd2b26293bac9363047
whereami0404 <whereami2048@gmail.com>
2024-11-02 14:59:13 +0900

    VACGOM-23 feat: 인증 관련 서비스, 토큰 관련 서비스 기능 구현
    AuthService: 인증과 관련된 서비스 기능 제공
    MemberTokenService: 유저 JWT 관련 서비스 기능 제공

---
+++ vacgom-api/src/main/kotlin/kr/co/vacgom/api/member/service/MemberTokenService.kt
@@ -0,0 +1,74 @@
+package kr.co.vacgom.api.member.service
+
+import com.auth0.jwt.interfaces.Claim
+import jakarta.servlet.http.HttpServletRequest
+import kr.co.vacgom.api.auth.jwt.JwtPayload
+import kr.co.vacgom.api.auth.jwt.JwtProperties
+import kr.co.vacgom.api.auth.jwt.JwtProvider
+import kr.co.vacgom.api.auth.jwt.TokenType
+import kr.co.vacgom.api.member.repository.MemberTokenRepository
+import org.springframework.stereotype.Service
+import java.time.Instant
+import java.util.*
+
+@Service
+class MemberTokenService(
+    private val jwtTokenProvider: JwtProvider,
+    private val jwtProperties: JwtProperties,
+    private val memberTokenRepository: MemberTokenRepository
+) {
+    fun createAccessToken(userId: Long): String {
+        val jwtPayLoad = JwtPayload(
+                iss = jwtProperties.issuer,
+                sub = TokenType.ACCESS_TOKEN.type,
+                exp = Date.from(Instant.now().plusSeconds(jwtProperties.accessTokenExpirationSec)),
+                privateClaims = mutableMapOf("userId" to userId)
+            )
+
+        return jwtTokenProvider.createToken(jwtPayLoad, jwtProperties.secret)
+    }
+
+    fun createRefreshToken(userId: Long): String {
+        val jwtPayLoad = JwtPayload(
+            iss = jwtProperties.issuer,
+            sub = TokenType.REFRESH_TOKEN.type,
+            exp = Date.from(Instant.now().plusSeconds(jwtProperties.refreshTokenExpirationSec)),
+            privateClaims = mutableMapOf("userId" to userId)
+        )
+
+        val refreshToken = jwtTokenProvider.createToken(jwtPayLoad, jwtProperties.secret)
+        memberTokenRepository.save(refreshToken, userId)
+
+        return refreshToken
+    }
+
+    fun resolveToken(token: String): Long {
+        val jwtPayload = jwtTokenProvider.verifyToken(token, jwtProperties.secret).getOrThrow()
+        val userIdClaim = jwtPayload.privateClaims["userId"] as Claim
+        return userIdClaim.asLong()
+    }
+
+    fun reIssueAccessToken(refreshToken: String): String {
+        val userId = resolveToken(refreshToken)
+        return createAccessToken(userId)
+    }
+
+    fun extractToken(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader(TOKEN_HEADER)
+
+        return if (bearerToken != null && bearerToken.startsWith(BEARER_PREFIX)) {
+            bearerToken.substring(BEARER_PREFIX.length)
+        } else {
+            null
+        }
+    }
+
+    fun deleteRefreshToken(userId: Long) {
+        memberTokenRepository.deleteTokenByUserId(userId)
+    }
+
+    companion object {
+        const val TOKEN_HEADER = "Authorization"
+        const val BEARER_PREFIX = "Bearer "
+    }
+}
