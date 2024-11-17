2ecfa82f3b9d0bd14608bfdf39b2f3526b276345
whereami2048 <whereami2048@gmail.com>
2024-11-17 20:24:12 +0900

    refactor: SecurityContextHolder 자체 구현 삭제

--- vacgom-api/src/main/kotlin/kr/co/vacgom/api/global/security/SecurityContext.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.vacgom.api.global.security
-
-class SecurityContext<T: Authentication>(
-    private val authentication: T
-) {
-    fun getAuthentication(): T {
-        return authentication
-    }
-}
