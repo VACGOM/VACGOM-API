01efc7c8d8b7906a576e4c4c41ee18ce0074c641
whereami0404 <whereami2048@gmail.com>
2024-12-15 19:36:01 +0900

    [VACGOM-98] feat: 초대 코드 엔티티 생성 일시 프로퍼티 추가

--- src/main/kotlin/kr/co/vacgom/api/invitation/domain/InvitationCode.kt
+++ src/main/kotlin/kr/co/vacgom/api/invitation/domain/InvitationCode.kt
@@ -1,10 +1,12 @@
 package kr.co.vacgom.api.invitation.domain
 
+import java.time.LocalDateTime
 import java.util.*
 
 data class InvitationCode(
     val key: String,
     val userId: UUID,
     val babyIds: List<UUID>,
-    val timeToLive: Long = 86400L
+    val timeToLive: Long = 86400L,
+    val createdAt: LocalDateTime = LocalDateTime.now()
 )
