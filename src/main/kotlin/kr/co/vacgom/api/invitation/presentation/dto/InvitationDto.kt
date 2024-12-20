e66854c4f0cfb0305394af531632335456d676a7
whereami0404 <whereami2048@gmail.com>
2024-12-20 23:14:16 +0900

    [VACGOM-70] feat: 초대 코드 API 명세 작성

--- src/main/kotlin/kr/co/vacgom/api/invitation/presentation/dto/InvitationDto.kt
+++ src/main/kotlin/kr/co/vacgom/api/invitation/presentation/dto/InvitationDto.kt
@@ -4,14 +4,15 @@ import io.swagger.v3.oas.annotations.media.Schema
 import kr.co.vacgom.api.invitation.domain.CareScope
 
 class InvitationDto {
-    @Schema(name = "InvitationDto.Request")
     class Request {
+        @Schema(name = "InvitationDto.Request.Create")
         data class Create(val careScope: CareScope)
+        @Schema(name = "InvitationDto.Request.Register")
         data class Register(val invitationCode: String)
     }
 
-    @Schema(name = "InvitationDto.Response")
     class Response {
+        @Schema(name = "InvitationDto.Response.Create")
         data class Create(val invitationCode: String)
     }
 }
