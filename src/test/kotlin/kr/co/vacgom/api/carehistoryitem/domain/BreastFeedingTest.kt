b8f9171f2875c35fbd6d04688ebd661aa684767d
whereami2048 <whereami2048@gmail.com>
2024-12-22 00:45:17 +0900

    [VACGOM-102] test: 육아 기록 아이템 생성 테스트 코드 수정

--- src/test/kotlin/kr/co/vacgom/api/carehistoryitem/domain/BreastFeedingTest.kt
+++ src/test/kotlin/kr/co/vacgom/api/carehistoryitem/domain/BreastFeedingTest.kt
@@ -2,8 +2,11 @@ package kr.co.vacgom.api.carehistoryitem.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kr.co.vacgom.api.baby.domain.Baby
+import kr.co.vacgom.api.baby.domain.enums.Gender
 import kr.co.vacgom.api.carehistoryitem.domain.enums.BreastDirection
 import kr.co.vacgom.api.carehistoryitem.domain.enums.CareHistoryItemType
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class BreastFeedingTest : FunSpec({
@@ -13,11 +16,19 @@ class BreastFeedingTest : FunSpec({
         val minutes = 10
         val breastDirection = BreastDirection.LEFT
 
+        val baby = Baby(
+            name = "백곰 아기",
+            profileImg = "아기 이미지",
+            gender = Gender.MALE,
+            birthday = LocalDate.now(),
+        )
+
         val breastFeeding = BreastFeeding(
             startTime = startTime,
             endTime = endTime,
             minutes = minutes,
             breastDirection = breastDirection,
+            baby = baby,
             executionTime = startTime,
             itemType = CareHistoryItemType.BATH
         )
--- src/test/kotlin/kr/co/vacgom/api/carehistoryitem/domain/SnackTest.kt
+++ src/test/kotlin/kr/co/vacgom/api/carehistoryitem/domain/SnackTest.kt
@@ -2,15 +2,27 @@ package kr.co.vacgom.api.carehistoryitem.domain
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kr.co.vacgom.api.baby.domain.Baby
+import kr.co.vacgom.api.baby.domain.enums.Gender
 import kr.co.vacgom.api.carehistoryitem.domain.enums.CareHistoryItemType
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class SnackTest : FunSpec({
     test("Snack 객체 정상 생성 테스트") {
         val memo = "과자 5봉지나 먹음. 크게 성장할 듯"
         val executionTime = LocalDateTime.now()
+
+        val baby = Baby(
+            name = "백곰 아기",
+            profileImg = "아기 이미지",
+            gender = Gender.MALE,
+            birthday = LocalDate.now(),
+        )
+
         val snack = Snack(
             memo = memo,
+            baby = baby,
             itemType = CareHistoryItemType.SNACK,
             executionTime = executionTime
         )
