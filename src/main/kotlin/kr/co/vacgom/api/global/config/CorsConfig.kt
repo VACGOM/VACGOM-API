55fc83ff67065c8e55e6b032ce7dce70e3205c7a
whereami2048 <whereami2048@gmail.com>
2025-01-06 02:44:52 +0900

    hotfix: cors 문제 해결

--- src/main/kotlin/kr/co/vacgom/api/global/config/CorsConfig.kt
+++ src/main/kotlin/kr/co/vacgom/api/global/config/CorsConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:80", "http://localhost:3000", "https://vacgom.co.kr")
+            .allowedOrigins("https://api-dev-v2.vacgom.co.kr", "http://localhost:80", "http://localhost:3000", "https://vacgom.co.kr")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
     }
