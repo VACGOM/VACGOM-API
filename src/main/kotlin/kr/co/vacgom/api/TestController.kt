8436148056ba3b64fdf3ea26516480b19ee3adc0
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2025-01-06 13:29:58 +0900

    chore: 테스트용 토큰 발급 API 구현 (#45)

--- src/main/kotlin/kr/co/vacgom/api/TestController.kt
+++ src/main/kotlin/kr/co/vacgom/api/TestController.kt
@@ -1,12 +1,15 @@
 package kr.co.vacgom.api
 
+import kr.co.vacgom.api.auth.oauth.enums.SocialLoginProvider
 import kr.co.vacgom.api.global.presentation.GlobalPath.BASE_V3
 import kr.co.vacgom.api.user.application.UserTokenService
 import kr.co.vacgom.api.user.repository.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
 @RestController
 @RequestMapping(BASE_V3 + "/TEST")
@@ -16,9 +19,25 @@ class TestController(
 ) {
 
     @PostMapping
-    fun test(): ResponseEntity<String> {
-        val user = userRepository.findAll().get(0)
-        val accessToken = userTokenService.createAccessToken(user.id, user.role)
-        return ResponseEntity.ok(accessToken)
+    fun test(@RequestParam tokenType: String): ResponseEntity<String> {
+        val user = userRepository.findAll()[0]
+
+        when (tokenType) {
+            "ACCESS" -> {
+                val accessToken = userTokenService.createAccessToken(user.id, user.role)
+                return ResponseEntity.ok(accessToken)
+            }
+            "REFRESH" -> {
+                val refreshToken = userTokenService.createRefreshToken(user.id)
+                return ResponseEntity.ok(refreshToken)
+            }
+            "REGISTER" -> {
+                val socialId = "testSocialId${Random(System.currentTimeMillis()).nextInt()}"
+                val registerToken = userTokenService.createRegisterToken(socialId, SocialLoginProvider.KAKAO.name)
+                return ResponseEntity.ok(registerToken)
+            }
+        }
+
+        return ResponseEntity.internalServerError().build()
     }
 }
