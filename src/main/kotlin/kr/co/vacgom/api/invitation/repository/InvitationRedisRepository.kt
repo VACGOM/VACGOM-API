4ef83a7723f60c995a614147e0b067bdf6510e79
whereami0404 <whereami2048@gmail.com>
2025-01-14 19:46:08 +0900

    feat: redis Repository초대코드 만료 여부 롤백 기능 구현

--- src/main/kotlin/kr/co/vacgom/api/invitation/repository/InvitationRedisRepository.kt
+++ src/main/kotlin/kr/co/vacgom/api/invitation/repository/InvitationRedisRepository.kt
@@ -18,14 +18,25 @@ class InvitationRedisRepository(
             .also { value -> redisTemplate.opsForValue().set(key, value, ttl, TimeUnit.SECONDS)}
     }
 
-    fun getAndDeleteInvitationCode(code: String): InvitationCode? {
-        val queryKey = INVITATION_CODE_KEY_PREFIX + code
-        val value = redisTemplate.opsForValue().getAndDelete(queryKey)
-
-        return objectMapper.readValue(value.toString(), InvitationCode::class.java)
+    fun getInvitationCodeAndUpdateExpired(code: String): InvitationCode? {
+        val key = INVITATION_CODE_KEY_PREFIX + code
+        return redisTemplate.execute { getInvitationCodeAndUpdateExpired(key, true) }
     }
 
-    companion object{
+    fun rollBackExpiredInvitationCode(code: String) {
+        val key = INVITATION_CODE_KEY_PREFIX + code
+        redisTemplate.execute { getInvitationCodeAndUpdateExpired(key, false) }
+    }
+
+    private fun getInvitationCodeAndUpdateExpired(key: String, expireStatus: Boolean): InvitationCode? {
+        val value = redisTemplate.opsForValue().get(key) ?: return null
+
+        return objectMapper.readValue(value.toString(), InvitationCode::class.java)
+            .apply { isExpired = expireStatus }
+            .also { redisTemplate.opsForValue().set(key, objectMapper.writeValueAsString(it)) }
+    }
+
+    companion object {
         const val INVITATION_CODE_KEY_PREFIX = "invitation_code:"
     }
 }
