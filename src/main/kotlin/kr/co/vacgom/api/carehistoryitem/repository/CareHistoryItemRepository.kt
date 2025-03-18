a1840d88f710d2f30caafd44437d62f8607169a4
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2025-03-18 15:09:24 +0900

    fix: 육아기록 조회 시 아기 존재하지 않는 경우 예외처리 (#64)

--- src/main/kotlin/kr/co/vacgom/api/carehistoryitem/repository/CareHistoryItemRepository.kt
+++ src/main/kotlin/kr/co/vacgom/api/carehistoryitem/repository/CareHistoryItemRepository.kt
@@ -1,13 +1,13 @@
 package kr.co.vacgom.api.carehistoryitem.repository
 
+import kr.co.vacgom.api.baby.domain.Baby
 import kr.co.vacgom.api.carehistoryitem.domain.CareHistory
 import kr.co.vacgom.api.carehistoryitem.domain.CareHistoryItem
 import kr.co.vacgom.api.carehistoryitem.domain.enums.CareHistoryItemType
 import java.time.LocalDate
-import java.util.*
 
 interface CareHistoryItemRepository {
     fun saveHistoryItem(item: CareHistoryItem)
-    fun findByBabyIdAndExecutionDate(babyId: UUID, executionDate: LocalDate): CareHistory
-    fun findByBabyIdAndExecutionDateAndItemType(babyId: UUID, executionDate: LocalDate, itemType: CareHistoryItemType): List<CareHistoryItem>
+    fun findByBabyAndExecutionDate(baby: Baby, executionDate: LocalDate): CareHistory
+    fun findByBabyAndExecutionDateAndItemType(baby: Baby, executionDate: LocalDate, itemType: CareHistoryItemType): List<CareHistoryItem>
 }
